27dcdcb314e081c4bfa3bece741ae553f7968ead
Redchin Daniil <redchindaniil@gmail.com>
2024-10-01 13:29:44 +0700

    Added libmaze setup

---
+++ libmaze/include/libmaze/maze.h
@@ -0,0 +1,6 @@
+#ifndef LIBMAZE_INCLUDE_MAZE_H_
+#define LIBMAZE_INCLUDE_MAZE_H_
+
+void hello_maze();
+
+#endif // LIBMAZE_INCLUDE_MAZE_H_
---
+++ libmaze/src/maze.c
@@ -0,0 +1,6 @@
+#include <libmaze/maze.h>
+#include <stdio.h>
+
+void hello_maze() {
+    printf("Hello, world!\n");
+}
