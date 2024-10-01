07b61301d0a4ba8004d469235c9821f43c44d98b
Redchin Daniil <redchindaniil@gmail.com>
2024-10-01 14:12:02 +0700

    Removed googletest from submodules

--- maze_gtk/src/main.c
+++ maze_gtk/src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
-int main() {
+void my_callback_function(GtkWidget *widget, gpointer data);
+
+int main(int argc, char** argv) {
     printf("Hello, world!\n");
+
+    GtkWidget *window;
+    GtkWidget *button;
+
+    gtk_init(&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    button = gtk_button_new_with_label("Click Me!");
+}
+
+void destroy(GtkWidget *widget, gpointer data) {
+   gtk_main_quit ();
 }
