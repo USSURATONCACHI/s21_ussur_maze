4be1e9b7af60b6a75e2f7262f98cbc4927bc2267
Redchin Daniil <redchindaniil@gmail.com>
2024-10-04 20:11:46 +0700

    Made prepare init call run only once

--- mazegtk/src/view/gtk_view.c
+++ mazegtk/src/view/gtk_view.c
@@ -1,16 +1,18 @@
 #include <mazegtk/view/gtk_view.h>
 #include <mazegtk/util/getref.h>
 
+#include <better_c_std/prettify.h>
+
 bool MgGtkView_is_fine(const MgGtkView* view) {
     return !view->is_stopped;
 }
 
 gboolean MgGtkView_startup_prepare(MgGtkView* view) {
-    
+    debugln("Sigma sigma");
     GtkStack* stack = GTK_STACK(gtk_builder_get_object(view->builder, "main_stack"));
     GtkWidget* show_maze_ui = GTK_WIDGET(gtk_builder_get_object(view->builder, "show_maze_ui"));
 
     gtk_stack_set_visible_child(stack, show_maze_ui);
 
-    return true;
+    return false;
 }
